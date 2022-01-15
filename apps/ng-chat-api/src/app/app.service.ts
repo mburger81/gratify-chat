ecb1bff2f9025adb49371dd267ddee7858ef5b1b
Michael Burger <michael.burger@orma-solutions.it>
2022-01-15 12:55:58 +0100

    chore(app): add ng-chat example app

--- apps/ng-chat-api/src/app/app.service.ts
+++ apps/ng-chat-api/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to ng-chat-api!' };
+  getHello(): Object {
+    return {title: 'Hello Youtube!'};
   }
 }
---
+++ apps/ng-chat-api/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'hard_to_guess_secret_123'
+    });
+  }
+
+  async validate(payload: any) {
+    return { ...payload.user };
+  }
+}
