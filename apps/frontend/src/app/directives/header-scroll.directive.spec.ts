4731d57c00b219606e7597783ee734658d16677a
Michael Burger <michael.burger@orma-solutions.it>
2022-01-20 21:04:43 +0100

    refactor(all): remove chat template sources

--- apps/frontend/src/app/directives/header-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HeaderScrollDirective } from './header-scroll.directive';
-
-describe('HeaderScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HeaderScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
