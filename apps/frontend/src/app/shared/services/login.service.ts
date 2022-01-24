a508e4ef37e5e3713f9e1b1f7be01df18e76ae86
Michael Burger <michael.burger@orma-solutions.it>
2022-01-24 13:27:05 +0100

    firebase(aut): use @robingenz/capacitor-firebase-authentication plugin

--- apps/frontend/src/app/shared/services/login.service.ts
+++ apps/frontend/src/app/shared/services/login.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth, GoogleAuthProvider, signInWithPopup, signOut } from '@angular/fire/auth';
+import { Auth, GoogleAuthProvider, signOut } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { FirebaseAuthentication } from '@robingenz/capacitor-firebase-authentication';
+import { AuthCredential, signInWithCredential } from 'firebase/auth';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
@@ -18,41 +20,43 @@ export class LoginService {
   ) { }
 
 
-  public loginWithGoogle(): Promise<void> {
-    return this.oAuthLogin(new GoogleAuthProvider());
+  public async loginWithGoogle(): Promise<void> {
+    // 1. Create credentials on the native layer
+    const result = await FirebaseAuthentication.signInWithGoogle();
+    // 2. Sign in on the web layer using the id token
+    const credential = GoogleAuthProvider.credential(result.credential?.idToken);
+    return this.signInWithCredential(credential);
   }
 
   public logout(): Promise<void> {
     return signOut(this.auth);
   }
 
-  private async oAuthLogin(provider: GoogleAuthProvider): Promise<void> {
+  private async signInWithCredential(credential: AuthCredential): Promise<void> {
 
-    // sign in
-    const credential = await signInWithPopup(this.auth, provider);
-    // const additionalInfo = getAdditionalUserInfo(credential);
+    const userCredential = await signInWithCredential(this.auth, credential);
+    // const additionalInfo = getAdditionalUserInfo(userCredential);
     // console.log('LoginService#oAuthLogin; additionalInfo:', additionalInfo);
 
-    firebase
 
     // create user in db
     // if (additionalInfo?.isNewUser) {
-      await firebase.database().ref("accounts/" + credential.user.uid)
+      await firebase.database().ref("accounts/" + userCredential.user.uid)
         .once('value').then((accounts) => {
           // this action when the user not created account,
           // it measn if not
           if (!accounts.val()) {
-            const userId = credential.user.uid
+            const userId = userCredential.user.uid
             // Set the image url link defualt
-            const img = credential.user.photoURL || "assets/profile.png";
+            const img = userCredential.user.photoURL || "assets/profile.png";
             // Set default description.
             const description = "Hello! I am a new Communicaters user.";
             const tempData = {
               img: img,
-              username: credential.user.displayName,
-              phoneNumber: credential.user.phoneNumber,
-              email: credential.user.email,
-              emailVerified: credential.user.emailVerified,
+              username: userCredential.user.displayName,
+              phoneNumber: userCredential.user.phoneNumber,
+              email: userCredential.user.email,
+              emailVerified: userCredential.user.emailVerified,
               description: description,
               nikeName:'',
               userId: userId,
@@ -60,18 +64,18 @@ export class LoginService {
               dateCreated: new Date().toString(),
               kyc: true
             }
-            this.angulaeDb.object("accounts/" + credential.user.uid).set(tempData).then(() => {
+            this.angulaeDb.object("accounts/" + userCredential.user.uid).set(tempData).then(() => {
               // this.loading.hide();
               this.router.navigateByUrl('/dashboard')
             }).then(() => {
-              this.angulaeDb.object("accounts/" + credential.user.uid).update({
+              this.angulaeDb.object("accounts/" + userCredential.user.uid).update({
                 status: "Online"
               })
             })
           } else {
             this.router.navigateByUrl('/dashboard').then(() => {
               // this.loading.hide();
-              this.angulaeDb.object("accounts/" + credential.user.uid).update({
+              this.angulaeDb.object("accounts/" + userCredential.user.uid).update({
                 status: "Online"
               })
             })
